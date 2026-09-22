d00d49d013c0040ff0b95d9b871fa642ae7efdb5
vm-ms <varun@magadhastudios.com>
2026-09-22 18:42:53 +0530

    settings changes

--- Assets/Racing Starter Kit/RSK Assets/Scripts/RaceFinish.cs
+++ Assets/Racing Starter Kit/RSK Assets/Scripts/RaceFinish.cs
@@ -69,6 +69,7 @@ namespace SpinMotion
 
         private void OnRaceTimeout()
         {
+            if (raceFinished) return;
             raceFinished = true;
             gameEvents.RaceFinishedEvent.Invoke(RaceFinishType.Timeout);
         }
